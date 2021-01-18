3191f6af035a99edacc93caeccb57469e2792f71
Vytautas Norkus <vytautas.norkus@hostinger.com>
2021-01-18 15:59:20 +0200

    Add poll message text and credit balance getters

--- Protocols/EPP/eppExtensions/nicbr-1.0/eppResponses/nicbrEppPollResponse.php
+++ Protocols/EPP/eppExtensions/nicbr-1.0/eppResponses/nicbrEppPollResponse.php
@@ -7,6 +7,20 @@ class nicbrEppPollResponse extends eppPollResponse
     /**
      * @return null|string
      */
+    public function getMessageText() {
+        return $this->queryPath('/epp:epp/epp:response/epp:msgQ/epp:msg/epp:txt');
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getMessageCreditBalance() {
+        return $this->queryPath('/epp:epp/epp:response/epp:msgQ/epp:msg/epp:creditBalance');
+    }
+    
+    /**
+     * @return null|string
+     */
     public function getDomainTicket()
     {
         return $this->queryPath('/epp:epp/epp:response/epp:extension/brdomain:' . $this->getMessageType() . 'Data/brdomain:ticketNumber');
